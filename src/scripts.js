3c3a31c133983b64a2481df93692cb35ba4caa0a
jsy20 <82422738+jsy20@users.noreply.github.com>
2021-05-08 10:31:13 -0700

    added demo, providers

--- src/scripts.js
+++ src/scripts.js
@@ -31,3 +31,27 @@ function myFunction() {
 	  }
 	}
   }
+
+
+/* Providers */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+document.getElementById('accordion').style.display = 'none';
+
